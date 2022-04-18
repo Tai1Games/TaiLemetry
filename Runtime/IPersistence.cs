988b8ed1d5555ec2d3ed7cfdac67045d25433ccc
Will7461 <43720307+Will7461@users.noreply.github.com>
2022-04-18 18:44:20 +0200

    borrado Persistence y Serializer (#10)

--- Runtime/IPersistence.cs
+++ Runtime/IPersistence.cs
@@ -7,7 +7,6 @@ namespace Tailemetry
 	//Interface to be implemented by different types of persistence
 	public interface IPersistence
 	{
-
 		public void Send(TrackerEv ev);
 
 		public void Flush();
