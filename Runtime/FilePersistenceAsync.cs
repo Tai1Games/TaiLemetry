a8cb3e11ae5f55f1bcbb62d77673e9b8392c9c8d
AdriAlvarez00 <adralv01@ucm.es>
2022-04-24 15:54:21 +0200

    Safe Threading

--- Runtime/FilePersistenceAsync.cs
+++ Runtime/FilePersistenceAsync.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 using System.Collections.Concurrent;
 using System.Threading;
 using System.IO;
+using System;
 
 namespace Tailemetry
 {
@@ -40,37 +41,56 @@ namespace Tailemetry
 			flushThread.Start(flushData);
 		}
 
+		private static StreamWriter OpenLogFile(string path,string header)
+        {
+			FileStream fs = null;
+			StreamWriter sw = null;
+            if (!File.Exists(path))
+            {
+				fs = File.Create(path);
+				sw = new StreamWriter(fs);
+				sw.WriteLine(header);
+				
+            }
+            else
+            {
+                try
+                {
+					fs = File.Open(path, FileMode.Append, FileAccess.Write, FileShare.None);
+					sw = new StreamWriter(fs);
+				}
+                catch (Exception e)
+                {
+					Debug.Log(e.Message);
+					//File is already in use
+					Debug.Log("Logfile en uso");
+					sw = null;
+                }
+            }
+			return sw;
+        }
+
 		private static void FlushQueue(object data)
 		{
 			FlushData flushData = (FlushData)data;
-			StreamWriter sw = new StreamWriter(flushData.path, append: true);
+			StreamWriter sw = OpenLogFile(flushData.path, flushData.serializer.GetHeader());
 			TrackerEv ev;
 
-			sw.WriteLine(flushData.serializer.GetHeader());
-
-			//In to avoid starvation, This is the ONLY place that can deque data
-			while (!flushData.queue.IsEmpty)
+			//If the file isnt available to write, sw will be null
+			if (sw != null)
 			{
-				flushData.queue.TryDequeue(out ev);
-				WriteFileEntry(flushData, sw, ev);
-				sw.WriteLine($"{flushData.serializer.Serialize(ev)},");
-			}
+				//In to avoid starvation, This is the ONLY place that can deque data
+				while (!flushData.queue.IsEmpty)
+				{
+					flushData.queue.TryDequeue(out ev);
+					sw.WriteLine($"{flushData.serializer.Serialize(ev)},");
+					Debug.Log(flushData.queue.Count);
+				}
 
-			sw.WriteLine(flushData.serializer.GetFooter());
+				sw.WriteLine(flushData.serializer.GetFooter());
+				sw.Close();
 
-			sw.Close();
-		}
-
-
-		private static void WriteFileEntry(FlushData flushData, StreamWriter sw, TrackerEv ev)
-		{
-			switch (flushData.serializer)
-			{
-				case JsonSerializer _:
-					
-					break;
-				default:
-					break;
+				Debug.Log("klose");
 			}
 		}
 	};
